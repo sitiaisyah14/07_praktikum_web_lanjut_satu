7d23bf031fb8a623d7ad19658a3b30c004739f59
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-28 11:42:14 +0700

    Setting nilai $fillable di Model Mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Mahasiswa extends Model
 {
     use HasFactory;
+
+    protected $table="mahasiswas"; // Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    public $timestamps= false;
+    protected  $primaryKey = 'nim'; // Memanggil isi DB Dengan primarykey
+
+    protected $fillable = [
+        'nim',
+        'nama',
+        'kelas',
+        'jurusan',
+        'no_handphone',
+    ];
+
 }
+
+
+
