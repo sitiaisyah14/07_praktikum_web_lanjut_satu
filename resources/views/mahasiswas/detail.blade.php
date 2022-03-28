89b2e8609286b362acb1e9b7cb0f13fcd6429e9e
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-03-28 14:46:15 +0700

    Modify view detail

--- resources/views/mahasiswas/detail.blade.php
+++ resources/views/mahasiswas/detail.blade.php
@@ -1,7 +1,6 @@
 @extends('mahasiswas.layout')
 
 @section('content')
-
 <div class="container mt-5">
     <div class="row justify-content-center align-items-center">
         <div class="card" style="width: 24rem;">
@@ -10,14 +9,14 @@
             </div>
             <div class="card-body">
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa>Nim}}</li>
-                    <li class="list-group-item"><b>Nama:</b>{{$Mahasiswa>Nama}}</li>
-                    <li class="list-group-item"><b>Kelas:</b>{{$Mahasiswa>Kelas}}</li>
-                    <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa>Jurusan}}</li>
-                    <li class="list-groupitem"><b>No_Handphone:</b>{{$Mahasiswa->No_Handphone}}</li>
+                    <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->nim}}</li>
+                    <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->nama}}</li>
+                    <li class="list-group-item"><b>Kelas: </b>{{$Mahasiswa->kelas}}</li>
+                    <li class="list-group-item"><b>Jurusan: </b>{{$Mahasiswa->jurusan}}</li>
+                    <li class="list-group-item"><b>No_Handphone: </b>{{$Mahasiswa->no_handphone}}</li>
                 </ul>
             </div>
-            <a class="btn btn-success mt3" href="{{ route('mahasiswas.index')}}">Kembali</a>
+            <a class="btn btn-success mt3" href="{{route('mahasiswa.index')}}">Kembali</a>
         </div>
     </div>
 </div>
