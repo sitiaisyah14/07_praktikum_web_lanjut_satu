8e71e2ea83578026f81b9f4921c2905930bd4ddc
sitiaisyah14 <sitiaisyah4110@gmail.com>
2022-04-18 09:03:05 +0700

    Js 10_Praktikum 1: Membuat Form Unggah Berkas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::resource('mahasiswa', MahasiswaController::class);
 
 Route::get('/nilai/{nim}', [MahasiswaController::class, 'nilai'])->name('nilai');
+
+Route::resource('articles', ArticleController::class);
